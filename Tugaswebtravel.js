8948bcf5f250373e8040779b4cf83028937434f9
widanaputra <116348599+widanaputra@users.noreply.github.com>
2022-10-21 18:33:42 +0800

    Delete Tugaswebtravel.js

--- Tugaswebtravel.js
+++
@@ -1,7 +0,0 @@
-const menu = document.querySelector('.menu-toggle');
-const menuLinks = document.querySelector('#menubtns');
-
-menu.addEventListener('click',function() {
-    menu.classList.toggle('is-active');
-    menuLinks.classList.toggle('active');
-})
